d4f4da9e8cd75fd40aa37ce86692658153efaf65
Wisdom Anthony <wizitendo10@gmail.com>
2018-05-04 09:02:58 +0100

    -g

--- app_server/controllers/api.js
+++ app_server/controllers/api.js
@@ -1,9 +1,14 @@
 var request = require('request');
+var axios = require('axios');
 
-var testurl = 'https://horizon-testnet.stellar.org'
+// To use the live network, set the hostname to 'horizon.stellar.org'
+var url = 'https://horizon-testnet.stellar.org' 
+
 var stellarSdk = require('stellar-sdk');
 stellarSdk.Network.useTestNetwork();
-var server = new stellarSdk.Server(testurl);
+// StellarSdk.Network.usePublicNetwork();
+
+var server = new stellarSdk.Server(url);
 var keypair = stellarSdk.Keypair; 
 
 var adminid = 'GDOFSE5LJMW4HRZXKBXP6VTDO2ZGLRMFHC4OWVMG2JQTI2XMDMCHC4RC'
@@ -17,7 +22,7 @@ var sendResponse = function(res,status,content){
 
 function getAdminSequenceNo(){
     var options = {
-        url: "https://horizon-testnet.stellar.org/accounts/"+adminid,
+        url: `${url}/${adminid}`,
         json: true
     }
     
@@ -30,6 +35,40 @@ function getAdminSequenceNo(){
     }
     
     request(options, callback)
+}
+
+var pay = function(res, sourceSecret, destinationid, amount){
+    var sourceSecretKey = sourceSecret;
+    var sourceKeyPair = stellarSdk.Keypair.fromSecret(sourceSecretKey)
+    var sourcePublicKey = sourceKeyPair.publicKey();
+    var receiverPublicKey = destinationid;
+    var server = new stellarSdk.Server('https://horizon-testnet.stellar.org');
+    stellarSdk.Network.useTestNetwork();
+    
+    server.loadAccount(sourcePublicKey)
+        .then(function(account){
+            var transaction = new stellarSdk.TransactionBuilder(account)
+                .addOperation(stellarSdk.Operation.payment({
+                    destination: receiverPublicKey,
+                    asset: stellarSdk.Asset.native(),
+                    amount: amount
+                })).build();
+        
+            transaction.sign(sourceKeyPair);
+        
+        server.submitTransaction(transaction)
+            .then(function(txnResult){
+                    var accountDetails = {
+                        id: sourcePublicKey,
+                        secret: sourceSecretKey,
+                        txn: txnResult
+                    }
+                    console.log(txnResult); 
+                    sendResponse(res, 200, accountDetails)
+                })
+            .catch(function(err){console.log('an error occurred',err.data.extras.result_codes)})
+        }).catch(function(err){console.log(err)})
+    
 }
 
 module.exports.createAdminAcct = function(req, res) {
@@ -77,40 +116,6 @@ module.exports.moreCoins = function(req, res) {
              
 }
 
-var pay = function(res, sourceSecret, destinationid, amount){
-    var sourceSecretKey = sourceSecret;
-    var sourceKeyPair = stellarSdk.Keypair.fromSecret(sourceSecretKey)
-    var sourcePublicKey = sourceKeyPair.publicKey();
-    var receiverPublicKey = destinationid;
-    var server = new stellarSdk.Server('https://horizon-testnet.stellar.org');
-    stellarSdk.Network.useTestNetwork();
-    
-    server.loadAccount(sourcePublicKey)
-        .then(function(account){
-            var transaction = new stellarSdk.TransactionBuilder(account)
-                .addOperation(stellarSdk.Operation.payment({
-                    destination: receiverPublicKey,
-                    asset: stellarSdk.Asset.native(),
-                    amount: amount
-                })).build();
-        
-            transaction.sign(sourceKeyPair);
-        
-        server.submitTransaction(transaction)
-            .then(function(txnResult){
-                    var accountDetails = {
-                        id: sourcePublicKey,
-                        secret: sourceSecretKey,
-                        txn: txnResult
-                    }
-                    console.log(txnResult); 
-                    sendResponse(res, 200, accountDetails)
-                })
-            .catch(function(err){console.log('an error occurred',err.data.extras.result_codes)})
-        }).catch(function(err){console.log(err)})
-    
-}
-
 module.exports.createAcct = function(req, res) {
     var pair = keypair.random();
     
@@ -139,17 +144,18 @@ module.exports.createAcct = function(req, res) {
 
 
 module.exports.checkBalance = function (req, res){
-    var id = adminid
+    var id = req.params.id
     
     server.loadAccount(id).then(function(account){
         var info = {
             desc: 'Balance for '+id,
-            balance: account.balances[0].balance
-        }
-        
-        sendResponse(res, 200, info)
-    })
+            balances: account.balances
+        }   
+
+         sendResponse(res, 200, info)
+    })   
 }
+
 
 module.exports.pay = function (req, res){
     var sourceSecret = req.params.secret //payers secret
@@ -158,3 +164,75 @@ module.exports.pay = function (req, res){
     
     pay(req, res, sourceSecret, destinationid, amount)
 }
+
+
+module.exports.transactionHistory = function (req,res){
+     var accountId =  req.params.id;
+     var payments = server.payments().forAccount(accountId);
+
+        server.transactions()
+        .forAccount(accountId)
+        .call()
+        .then(function (page) {
+            console.log('Page 1: ');
+            console.log(page.records);
+             sendResponse(res, 200, page.records);
+            return page.next();
+        })
+        .then(function (page) {
+            console.log('Page 2: ');
+            console.log(page.records);
+            sendResponse(res, 200, page.records);
+        })
+        .catch(function (err) {
+            console.log(err);
+                sendResponse(res, 500, err);
+        });
+}
+
+
+// Show all Offers like buy and sell made by an account on stellar blockchain
+module.exports.accountOffers = function (req,res) {
+    // body...
+     var accountId =  req.params.id;
+
+    server.offers('accounts', accountId)
+  .call()
+  .then(function (offerResult) {
+    console.log(offerResult);
+     sendResponse(res, 200, offerResult);
+  })
+  .catch(function (err) {
+    console.error(err);
+     sendResponse(res, 300, err);
+  });
+}
+
+
+
+module.exports.trade = function (req,res) {
+    // body...
+    var base_asset_type = 'native'
+    var base_asset_code = ''
+    var base_asset_issuer = ''
+    var counter_asset_type = 'credit_alphanum4'
+    var counter_asset_code = 'USD'
+    var counter_asset_issuer = 'GA2HGBJIJKI6O4XEM7CZWY5PS6GKSXL6D34ERAJYQSPYA6X6AI7HYW36'
+    var link = 'https://horizon.stellar.org/trade_aggregations?base_asset_type=native&counter_asset_code=SLT&counter_asset_issuer=GCKA6K5PCQ6PNF5RQBF7PQDJWRHO6UOGFMRLK3DYHDOI244V47XKQ4GP&counter_asset_type=credit_alphanum4&limit=200&order=asc&resolution=3600000&start_time=1517521726000&end_time=1517532526000'
+   // var link = `${url}/trade_aggregations?base_asset_type=${base_asset_type}&base_asset_code=${base_asset_code}&base_asset_issuer=${base_asset_issuer}&counter_asset_type=${counter_asset_type}&counter_asset_code=${counter_asset_code}&counter_asset_issuer=${counter_asset_issuer}&order=asc&resolution=3600000&start_time=1517521726000&end_time=1517532526000`
+    console.log(link);
+
+    axios.get(link)
+        .then(
+            function (trade) {
+               //console.log(trade);
+              //sendResponse(res, 200, trade);
+              res.status(200).json(trade);
+        })
+        .catch(error => {
+            res.status(500).send(error);
+        });  
+
+}
+
+
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -13,11 +13,19 @@ router.get('/api/createAcct', api.createAcct);
 
 router.get('/api/createAdminAcct', api.createAdminAcct);
 
-router.get('/api/checkBalance/:id', api.checkBalance);
+router.get('/api/moreCoins', api.moreCoins);
+
+router.get('/api/account/transHistory/:id', api.transactionHistory);
+
+router.get('/api/account/offers/:id', api.accountOffers);
+
+router.get('/api/account/checkBalance/:id', api.checkBalance);
 
 router.get('/api/pay/:secret/:id/:amount', api.pay);
 
-router.get('/api/moreCoins', api.moreCoins);
+router.get('/api/trade', api.trade);
+
+
 
 
 module.exports = router;
